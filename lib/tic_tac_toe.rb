80bb6d3a103832005b49ff87e9a2dbe929fb9e4b
Jason Leach <jlspursfan@gmail.com>
2019-04-24 19:58:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = (input.to_i - 1)
-  
+
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
