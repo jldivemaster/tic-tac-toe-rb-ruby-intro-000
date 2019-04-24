c022f6ee5b253ab82d128bf1772598e0537e6374
Jason Leach <jlspursfan@gmail.com>
2019-04-24 20:44:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,8 +18,12 @@ def turn_count(board)
 end
 
 
-def current_player()
-  turn_count(board) % 2 == 0 ? "X" : "O"
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end 
 end
 
 
