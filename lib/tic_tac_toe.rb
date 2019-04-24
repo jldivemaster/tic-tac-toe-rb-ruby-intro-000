378ab6b27d3b56dfdeffb971ac22d2c7ea2fa5e8
Jason Leach <jlspursfan@gmail.com>
2019-04-24 20:23:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,10 +26,10 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = (input.to_i - 1)
+  index = input_to_index(input)
 
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index)
     display_board(board)
   else
     puts "Invalid Move"
