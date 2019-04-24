4b5d44c1034568674bcb416298028ffc23b398ab
Jason Leach <jlspursfan@gmail.com>
2019-04-24 20:42:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,6 +7,22 @@ def display_board(board)
 end
 
 
+def turn_count(board)
+  count = 0
+  board.each do |ele|
+    if (ele == "X") || (ele == "O")
+      count += 1
+    end
+  end
+  return count
+end
+
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
@@ -37,20 +53,7 @@ def turn(board)
   end
 end
 
-def turn_count(board)
-  count = 0
-  board.each do |ele|
-    if (ele == "X") || (ele == "O")
-      count += 1
-    end
-  end
-  return count
-end
 
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
